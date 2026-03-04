3bc2f110f083b6afd283a7d876c58c32858abb41
Arthur Nakayama <61486792+ATKNK@users.noreply.github.com>
2026-03-04 14:46:53 -0300

    Feat: Created Side Panel component

---
+++ NeoKarte/src/components/sidePanel/SidePanel.jsx
@@ -0,0 +1,27 @@
+import './SidePanel.css';
+
+function SidePanel(props) {
+    return (
+        <div className='sidePanelMain'>
+            <header className='sidePanelYear'>{props.year}</header>
+            <div className='countryPanel'>
+
+                <p className='countryName'>{props.name}</p>
+                <p className='countryName'>{props.nameLocal}</p>
+
+                <img className='countryFlag' src='https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_Austria.svg'></img>
+                
+                <p className='panelLabel'>Capital:</p>
+                <p>{props.capital}</p>
+                
+                <p className='panelLabel'>Government:</p>
+                <p>{props.govt}</p>
+
+                <p>{props.leader}</p>
+                <img className='leaderPortrait' src='https://upload.wikimedia.org/wikipedia/commons/7/79/Hans_Burgkmair_d._%C3%84._%28zugeschr.%29_-_Bildnis_Kaiser_Friedrich_III.jpg'></img>
+            </div>
+        </div>
+    )
+}
+
+export default SidePanel
